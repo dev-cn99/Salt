acb0c9d88404b72403b6c6df08f8a581a3b0f80d
D <76818592+dev-cn99@users.noreply.github.com>
2021-02-20 16:30:59 +0900

    Update salt.js

--- salt.js
+++ salt.js
@@ -4,7 +4,7 @@ const getSalt = () => {
 
     let str = new java.lang.StringBuffer();
 
-    for (let i in str) str.append(text[new SecureRandom().nextInt(text.length)|0]);
+    for (let i in str) str.append(text[new java.security.SecureRandom().nextInt(text.length)|0]);
 
     return string(str).substring(0, 5);
 
